be2dea1d2fb0dcb8f7c8be367136b12b7bd98565
selevanm <selevanm@gmail.com>
2025-09-25 08:51:04 -0500

    vs autoscale

--- main.tf
+++ main.tf
@@ -52,6 +52,7 @@ module "autoscaling" {
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "blog-alb"
 
@@ -63,27 +64,28 @@ module "blog_alb" {
   # Security Group
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
+  http_tcp_listeners = [
+    {
+      port = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
+      target_group_index = 0
+    }
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = HTTP"
+      backend_port = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
       }
     }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
+  ]
 
   tags = {
     Environment = "dev"
