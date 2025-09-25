f1add59e737e27677b28d8823e5bd4fb584452bd
selevanm <selevanm@gmail.com>
2025-09-25 08:58:33 -0500

    more err

--- main.tf
+++ main.tf
@@ -42,12 +42,12 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.blog_vpc.subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
   #target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type      = var.instance_type
 }
 
 module "blog_alb" {
@@ -78,12 +78,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
