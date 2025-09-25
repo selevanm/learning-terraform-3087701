5f249c67b2dbb3d3b65fa4e8e1548ec35d36c776
selevanm <selevanm@gmail.com>
2025-09-25 08:38:51 -0500

    arns

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.subnets
-  target_group_arns = module.blog_alb.target_group_arns
+  #target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
