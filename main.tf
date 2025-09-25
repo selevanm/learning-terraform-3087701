77145c038b481e1b54f484aca2dcb4d4ce2c41a3
selevanm <selevanm@gmail.com>
2025-09-25 08:25:23 -0500

    clean up

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
 
   # Security Group
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     ex-http-https-redirect = {
